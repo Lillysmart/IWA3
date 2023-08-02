a1f7b123747731ab74ba019e5d3df071112ddeb4
Lillysmart <137174363+Lillysmart@users.noreply.github.com>
2023-08-02 14:18:42 +0200

    Add files to main folder

--- scripts.js
+++
@@ -1,7 +0,0 @@
-// scripts.js
-
-import company form 'configuration'
-import year form 'configuration'
-
-const message = '© ' + company + ' (' + year + ')'
-document.querySelector('footer').innerText = message
