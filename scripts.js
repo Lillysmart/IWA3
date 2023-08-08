bd4f83830f0d9390daf342d41a4a2e72eb14a4ad
Lillysmart <137174363+Lillysmart@users.noreply.github.com>
2023-08-08 15:31:50 +0200

    Final commit

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
 // scripts.js
 
-import {company} from '/configure.js';
+import {company} from './configure.js';
 
 
-import {year }from '/Configure.js';
+import {year }from './configure.js';
 
 const message = '© ' + company + ' (' + year + ')'
 document.querySelector('footer').innerText = message
